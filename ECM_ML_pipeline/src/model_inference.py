d993036ecf796f3af1144be40f0864a5b0693b5a
SushankMishra <sushankiitd@gmail.com>
2025-10-21 19:07:58 +0530

    Made Some changes to accommodate the new pipeline

--- ECM_ML_pipeline/src/model_inference.py
+++
@@ -1,5 +0,0 @@
-import joblib
-
-def predict_parameters(X_new, model_path):
-    model = joblib.load(model_path)
-    return model.predict(X_new)
--- ECM_ML_pipeline/src/utils.py
+++
@@ -1,3 +0,0 @@
-import numpy as np
-def mean_percentage_absolute_error(y_true, y_pred):
-    return np.mean(np.abs((y_true - y_pred) / y_true)) * 100
